6e33f7e219232e44e0a20232878377c914fe5394
tyang82 <tyang82@gatech.edu>
2018-02-21 10:51:05 -0500

    added query code for incrementing each task

--- app.py
+++ app.py
@@ -71,17 +71,17 @@ def webhook():
         if '!points' in text:
             send_wreck_message("Track - %.1f, Gym - %.1f, Throw - %.1f, Swim - %.1f, Pickup - %.1f, Biking - %.1f" % (TRACK_POINTS, GYM_POINTS, THROW_POINTS, SWIM_POINTS, PICKUP_POINTS, BIKING_POINTS))
         if '!gym' in text:
-            handle_workouts(data, GYM_POINTS)
+            handle_workouts(data, GYM_POINTS, 'gym')
         if '!throw' in text:
-            handle_workouts(data, THROW_POINTS)
+            handle_workouts(data, THROW_POINTS, 'throws')
         if '!swim' in text:
-            handle_workouts(data, SWIM_POINTS)
+            handle_workouts(data, SWIM_POINTS, 'swim')
         if '!track' in text:
-            handle_workouts(data, TRACK_POINTS)
+            handle_workouts(data, TRACK_POINTS, 'track')
         if '!bike' in text:
-            handle_workouts(data, BIKING_POINTS)
+            handle_workouts(data, BIKING_POINTS, 'bike')
         if '!pickup' in text:
-            handle_workouts(data, PICKUP_POINTS)
+            handle_workouts(data, PICKUP_POINTS, 'pickup')
         if '!leaderboard' in text: #display the leaderboard for who works out the most
             print_stats(2, True)
         if '!heatcheck' in text:
@@ -157,7 +157,7 @@ def webhook():
 def send_wreck_message(msg):
     send_message(msg, os.getenv("WRECK_BOT_ID"))
 
-def handle_workouts(data, addition):
+def handle_workouts(data, addition, task):
     if len(data['attachments']) > 0:
         # attachments are images or @mentions
         ids = []
@@ -179,7 +179,7 @@ def handle_workouts(data, addition):
             names.append(data['name'])
             ids.append(data['user_id'])
             send_debug_message(str(names))
-            num = add_to_db(names, addition, ids)
+            num = add_to_db(names, addition, ids, task)
             if num == len(names):
                 like_message(data['group_id'], data['id'])
 
@@ -260,7 +260,7 @@ def like_message(group_id, msg_id):
     request = Request(url, urlencode(data).encode())
     urlopen(request)
 
-def add_to_db(names, addition, ids): #add "addition" to each of the "names" in the db
+def add_to_db(names, addition, ids, task): #add "addition" to each of the "names" in the db
     cursor = None
     conn = None
     num_commited = 0
@@ -279,6 +279,10 @@ def add_to_db(names, addition, ids): #add "addition" to each of the "names" in t
             cursor.execute(sql.SQL(
                 "UPDATE wreck_data SET num_workouts = num_workouts+1, workout_score = workout_score+%s, last_post = now() WHERE id = %s"),
                 (str(addition), ids[x],))
+            cursor.execute(sql.SQL(
+                "UPDATE wreck_data SET  %s = %s+%s, last_post = now() WHERE id = %s"),
+                (task, task,str(1), ids[x],))
+
             if cursor.rowcount == 0: #If a user does not have an id yet
                 cursor.execute(sql.SQL(
                     "UPDATE wreck_data SET num_workouts = num_workouts+1, workout_score = workout_score+%s, last_post = now(), id = %s WHERE name = %s"),
@@ -294,6 +298,8 @@ def add_to_db(names, addition, ids): #add "addition" to each of the "names" in t
             cursor.close()
             conn.close()
         return num_commited
+
+
 
 
 
